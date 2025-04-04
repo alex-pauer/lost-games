d8947f4eca0502e186b6411ada2acf4bb6eced1e
FestikUwU <heorhii.tykhonov@cieep.com>
2025-04-04 12:09:12 +0200

    test

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+// home.component.ts
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+}
+
+interface Slide {
+  image: string;
+  alt: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  // Your component logic here
+  currentSlide = 0;
+  
+  slides: Slide[] = [
+    { image: 'assets/images/pc/slider/fable.webp', alt: 'Fable' },
+    // ... other slides
+  ];
 
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    // Initialization code
+  }
+
+  // Your methods here
+  nextSlide(): void {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+  }
 }
