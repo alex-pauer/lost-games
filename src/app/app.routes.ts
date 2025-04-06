ff006269063ad763545cda3e483b08f116d52378
FestikUwU <heorhii.tykhono@cieep.com>
2025-04-06 16:32:05 +0200

    sigo tener problemas con imagines pero css ya funcion bien

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,38 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { 
     path: '',
-    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+    loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
+    title: 'HOME'
   },
   {
     path: 'pc-page',
-    loadComponent: () => import('./pc-page/pc-page.component').then(m => m.PcPageComponent)
-  }
+    loadComponent: () => import('./pc-page/pc-page.component').then(m => m.PcPageComponent),
+    title: 'PC'
+  },
+  {
+    path: 'playstation',
+    loadComponent: () => import('./playstation-page/playstation-page.component').then(m => m.PlaystationPageComponent),
+    title: 'PlayStation'
+  },
+  { 
+    path: 'nintendo',
+    loadComponent: () => import('./nintendo-page/nintendo-page.component').then(m => m.NintendoPageComponent),
+    title: 'Nintendo'
+  },
+  {
+  path: 'xbox',
+    loadComponent: () => import('./xbox-page/xbox-page.component').then(m => m.XboxPageComponent),
+    title: 'Xbox'
+  },
+  { 
+    path: 'cart',
+    loadComponent: () => import('./cesta-page/cesta-page.component').then(m => m.CestaPageComponent),
+    title: 'Cesta'
+  },
+  { 
+    path: 'account',
+    loadComponent: () => import('./user-page/user-page.component').then(m => m.UserPageComponent),
+    title: 'Cuenta'
+  },
+  { path: '**', redirectTo: '' }
 ];
--- src/app/playstation-page/playstation-page.component.ts
+++ src/app/playstation-page/playstation-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-playstation-page',
-  imports: [],
+  standalone: true, 
+  imports: [CommonModule],
   templateUrl: './playstation-page.component.html',
   styleUrl: './playstation-page.component.css'
 })
