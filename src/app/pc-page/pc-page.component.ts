91bc2257ae6f33091afb365ee0de970fb3569e05
alex-pauer <alejandro.paumasip@cieep.com>
2025-04-22 15:35:28 +0200

    COMPONENTE SLIDER DE ANGULAR CON BOOTSRAP!!!

--- src/app/pc-page/pc-page.component.ts
+++ src/app/pc-page/pc-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { SliderComponent } from '../slider/slider.component'; 
 
 
 @Component({
   selector: 'app-pc-page',
   standalone: true, 
-  imports: [CommonModule, ProductCardComponent], 
+  imports: [CommonModule, ProductCardComponent, SliderComponent], 
   templateUrl: './pc-page.component.html',
   styleUrls: ['./pc-page.component.css']
 })
