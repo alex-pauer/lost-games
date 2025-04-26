91a5c8d065d2924c25a8119db65fae9d62b4dc89
FestikUwU <heorhii.tykhono@cieep.com>
2025-04-26 14:20:04 +0200

    hi boys

--- src/app/nintendo-page/nintendo-page.component.ts
+++ src/app/nintendo-page/nintendo-page.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { SliderComponent } from '../slider/slider.component';
 
 
 @Component({
   selector: 'app-nintendo-page',
   standalone: true,
-  imports: [CommonModule, ProductCardComponent],
+  imports: [CommonModule, ProductCardComponent, SliderComponent],
   templateUrl: './nintendo-page.component.html',
   styleUrl: './nintendo-page.component.css'
 })
-export class NintendoPageComponent { }
+export class NintendoPageComponent { 
+  pcSlides = [
+    '/assets/nintendo/Metroid-Prime-4-Beyond.webp',
+        '/assets/nintendo/ns2.webp',
+        '/assets/nintendo/xenobladeX.jpg'
+  ];
+}
 
