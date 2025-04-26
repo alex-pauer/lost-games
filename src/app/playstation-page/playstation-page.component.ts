74006a9a568d03f3ff86de07dbf7ee11f2c28385
FestikUwU <heorhii.tykhono@cieep.com>
2025-04-26 13:50:59 +0200

    pizdec nahuy blyat

--- src/app/playstation-page/playstation-page.component.ts
+++ src/app/playstation-page/playstation-page.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { SliderComponent } from '../slider/slider.component'; 
 
 @Component({
   selector: 'app-playstation-page',
   standalone: true, 
-  imports: [CommonModule, ProductCardComponent],
+  imports: [CommonModule, ProductCardComponent, SliderComponent],
   templateUrl: './playstation-page.component.html',
   styleUrl: './playstation-page.component.css'
 })
 export class PlaystationPageComponent {
-
+  pcSlides = [
+    '../../../assets/play/slider/ds2.jpg',
+    '../../../assets/play/slider/ghostOfYotei.webp',
+    '/../../../assets/play/slider/LobeznoGame.jpg'
+  ];
 }
--- src/app/xbox-page/xbox-page.component.ts
+++ src/app/xbox-page/xbox-page.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCardComponent } from '../product-card/product-card.component'; 
+import { SliderComponent } from '../slider/slider.component';
 
 @Component({
   selector: 'app-xbox-page',
   standalone: true,
-  imports: [CommonModule, ProductCardComponent],
+  imports: [CommonModule, ProductCardComponent, SliderComponent],
   templateUrl: './xbox-page.component.html',
   styleUrls: ['./xbox-page.component.css']
 })
 export class XboxPageComponent {
-
+  pcSlides = [
+    '/assets/xbox/PerfectDark.jpg',
+    '/assets/xbox/gears-of-war-e-day.webp',
+    '/assets/xbox/doomTDA.jpg'
+  ];
 }
